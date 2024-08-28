e39c1daccd237005dd6ebe990b4fde81425ddabf
ApsaraWitharana <asachini095@gmail.com>
2024-08-28 12:31:48 +0530

    update-token fronend-dashboard-set Authorization bearer token

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,6 +3,27 @@
 $(document).ready(function (){
     let token = localStorage.getItem("token");
     if (token){
+ //set hii massage-append to dashboard
+        $.ajax({
+            url: "http://localhost:8080/api/v1/blog/newMethod",
+            method: "GET",
+            contentType: "application/json",
+            headers:{
+                "Authorization":"Bearer "+token
+            },
+
+            //error handling
+            success: function (response) {
+                console.log(response)
+                // Append the token to the dashboard
+                $('<p>'+response+'<p>').appendTo('body')
+            },
+            error: function (error) {
+                console.log(error)
+
+            }
+
+        })
 
     }else {
         window.location.href="signin.html"
