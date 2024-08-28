c8d83daf8b7cc57d4967e563c4a661b69ff63887
ApsaraWitharana <asachini095@gmail.com>
2024-08-28 10:56:43 +0530

    update-token fronend-signin

--- js/auth.js
+++ js/auth.js
@@ -1,50 +1,67 @@
-function userRegistation(){
+function userRegistation() {
     console.log("click!!")
     //define variable
 
-    let  name = $('#username').val(); // get value
-    let  password = $('#login-pass').val(); // get value
-    let  email = $('#login-email').val(); // get value
-    console.log(name,password,email);
+    let name = $('#username').val(); // get value
+    let password = $('#login-pass').val(); // get value
+    let email = $('#login-email').val(); // get value
+    console.log(name, password, email);
 
     //create ajax request
-
     $.ajax({
         url: "http://localhost:8080/api/v1/auth/register",
+        method: "POST",
+        contentType: "application/json",
+        "data": JSON.stringify({
+            "name": name,
+            "password": password,
+            "email": email
+        }),
+        //error handling
+        success: function (response) {
+            console.log(response.data.token)
+            localStorage.setItem("token", response.data.token)
+            alert("save successfully!!")
+        },
+        error: function (error) {
+            console.log(error)
+            alert("save Unsuccessfully!!")
+        }
+
+    })
+}
+
+
+    function userLogin(){
+        console.log("click!")
+
+        let  password = $('#login-pass').val(); // get value
+        let  email = $('#login-email').val(); // get value
+
+ // create ajax authenticate
+    $.ajax({
+        url: "http://localhost:8080/api/v1/auth/authenticate",
         method:"POST",
         contentType:"application/json",
         "data":JSON.stringify({
-            "name":name,
             "password":password,
             "email":email
         }),
         //error handling
         success:function (response){
-            console.log(response)
+            console.log(response.data.token)
+            localStorage.setItem("token", response.data.token)
             alert("save successfully!!")
-    },
+        },
         error:function (error){
             console.log(error)
             alert("save Unsuccessfully!!")
         }
 
     })
-
-    // $.ajax({
-    //     type: "POST",
-    //     url: "http://localhost:8080/api/v1/auth/register",
-    //     data: JSON.stringify({ username: "user", password: "password" }),
-    //     contentType: "application/json",
-    //     success: function(response) {
-    //         console.log("User registered successfully!", response);
-    //     },
-    //     error: function(xhr, status, error) {
-    //         console.error("Error:", error);
-    //     }
-    // });
-
-
-
-
-
 }
+
+
+
+
+
